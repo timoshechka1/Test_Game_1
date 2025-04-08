4a503c5eba701691ddad28073789cf4dcc482e33
timoshechka1 <ttimoshechka@gmail.com>
2025-04-08 13:41:17 +0300

    fix: Correct closing of the program window

--- main.py
+++ main.py
@@ -3,5 +3,12 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 300))
 
-while True:
+running = True
+
+while running:
     pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
