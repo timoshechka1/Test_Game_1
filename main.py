149f613d4a75b1ed3e8bd58106771a664a421be1
timoshechka1 <ttimoshechka@gmail.com>
2025-04-10 22:55:29 +0300

    feat: Adding static animation to the  main character

--- main.py
+++ main.py
@@ -6,12 +6,35 @@ pygame.display.set_caption("Test Game 1")
 pygame.display.set_icon(pygame.image.load("images/icon.png"))
 
 background = pygame.image.load("images/background.png")
-player = pygame.image.load("images/player_movement_left/player_movement_left_1.png")
+moving_left = [
+    pygame.image.load("images/player_movement_left/player_movement_left_1.png"),
+    pygame.image.load("images/player_movement_left/player_movement_left_2.png"),
+    pygame.image.load("images/player_movement_left/player_movement_left_3.png"),
+    pygame.image.load("images/player_movement_left/player_movement_left_4.png"),
+    pygame.image.load("images/player_movement_left/player_movement_left_5.png"),
+    pygame.image.load("images/player_movement_left/player_movement_left_6.png")
+]
+moving_rigth = [
+    pygame.image.load("images/player_movement_right/player_movement_right_1.png"),
+    pygame.image.load("images/player_movement_right/player_movement_right_2.png"),
+    pygame.image.load("images/player_movement_right/player_movement_right_3.png"),
+    pygame.image.load("images/player_movement_right/player_movement_right_4.png"),
+    pygame.image.load("images/player_movement_right/player_movement_right_5.png"),
+    pygame.image.load("images/player_movement_right/player_movement_right_6.png")
+]
+
+player_anim_count = 0
 
 running = True
 while running:
     screen.blit(background, (0, 0))
-    screen.blit(player, (300, 350))
+    screen.blit(moving_rigth[player_anim_count], (300, 350))
+
+    if player_anim_count == 5:
+        player_anim_count = 0
+    else:
+        player_anim_count += 1
+
     pygame.display.update()
 
     for event in pygame.event.get():
