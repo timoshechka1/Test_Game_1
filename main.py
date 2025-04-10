5daf22dffb1486655b6e65716acc4440bb3bd439
timoshechka1 <ttimoshechka@gmail.com>
2025-04-10 22:39:43 +0300

    feat: Adding a background to the game and adding the main character to the game

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((600, 300))
+screen = pygame.display.set_mode((600, 476))
 pygame.display.set_caption("Test Game 1")
 pygame.display.set_icon(pygame.image.load("images/icon.png"))
 
+background = pygame.image.load("images/background.png")
+player = pygame.image.load("images/player_movement_left/player_movement_left_1.png")
+
 running = True
 while running:
+    screen.blit(background, (0, 0))
+    screen.blit(player, (300, 350))
+    pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
