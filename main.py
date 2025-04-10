c939fa7ad58917937467648d307a73cdadf59086
timoshechka1 <ttimoshechka@gmail.com>
2025-04-10 21:47:41 +0300

    fix: Code cleaning

--- main.py
+++ main.py
@@ -5,14 +5,8 @@ screen = pygame.display.set_mode((600, 300))
 pygame.display.set_caption("Test Game 1")
 pygame.display.set_icon(pygame.image.load("images/icon.png"))
 
-myfont = pygame.font.Font("fonts/Roboto-Black.ttf", 40)
-text_surface = myfont.render('timoshechka1', False, 'Red')
-
 running = True
 while running:
-    pygame.draw.circle(screen, 'Red', (250, 150), 30)
-    pygame.display.update()
-    screen.blit(text_surface, (10, 10))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
