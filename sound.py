9c417eb5515e9bbb32f0113f895461fb6821493c
timoshechka1 <ttimoshechka@gmail.com>
2025-04-15 20:24:55 +0300

    fix: Added documentation to modules sound.py

--- sound.py
+++ sound.py
@@ -1,13 +1,40 @@
 import pygame
 import settings
 
+# Initialize pygame mixer module for audio playback
 pygame.mixer.init()
 
+# Load background music audio file
 background_music = pygame.mixer.Sound(settings.BACKGROUND_MELODY)
+# Set default volume level from settings
 background_music.set_volume(settings.BACKGROUND_MELODY_VOLUME)
 
 def play_background_music():
+    """
+    Starts playing the background music in an infinite loop.
+
+    Args:
+        loops (int): Number of times to repeat the sound.
+                   -1 (default) for infinite looping.
+                   0 plays once, 1 plays twice, etc.
+
+    Note:
+        - Uses the pre-loaded background_music Sound object
+        - Volume is controlled by settings.BACKGROUND_MELODY_VOLUME
+        - Music will loop continuously by default
+    """
     background_music.play()
 
 def stop_background_music():
+    """
+    Stops the currently playing background music immediately.
+
+    Effects:
+        - Halts playback of the background_music Sound object
+        - Can be restarted with play_background_music()
+
+    Note:
+        - Does not reset playback position
+        - Subsequent play() calls will restart from beginning
+    """
     background_music.stop()
